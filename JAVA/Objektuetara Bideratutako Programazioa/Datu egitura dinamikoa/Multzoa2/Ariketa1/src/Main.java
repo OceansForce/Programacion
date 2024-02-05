6af0f26987908aa2df0212ff093f2dc37f724735
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-02-05 09:53:14 +0100

    Datu dinamikoen 2.multzoa 1 Ariketa
    1 ariketa hasten

---
+++ JAVA/Objektuetara Bideratutako Programazioa/Datu egitura dinamikoa/Multzoa2/Ariketa1/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Sartu zenbaki oso positiboa bat:");
+        int zenb1= sc.nextInt();
+        int ondarra;
+        int lehenak=zenb1;
+        int erantzuna=zenb1;
+        System.out.println("Hau da "+zenb1+" zenbakiaren deskonposaketa");
+
+        for (int i=1; i<=zenb1; i++){
+
+            ondarra= zenb1%i;
+
+            if (ondarra==0){
+                lehenak=lehenak/i;
+
+
+                for (int b=0; b==0;b++ ){
+                    erantzuna=erantzuna/lehenak;
+                    System.out.println(lehenak);
+                }
+            }
+        }
+        sc.close();
+    }
+}
