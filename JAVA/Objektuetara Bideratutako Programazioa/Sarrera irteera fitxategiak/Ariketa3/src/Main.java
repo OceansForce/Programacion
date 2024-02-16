c0d0f312b9e3dd599261c518ac2a0adf7e17f693
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-02-16 09:53:30 +0100

    Lanak egiten eta 4. Ariketa egiten

--- JAVA/Objektuetara Bideratutako Programazioa/Sarrera irteera fitxategiak/Ariketa3/src/Main.java
+++ JAVA/Objektuetara Bideratutako Programazioa/Sarrera irteera fitxategiak/Ariketa3/src/Main.java
@@ -2,19 +2,37 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-       File f1 = new File(".\\fitx1.txt");
+       try {
 
-        System.out.println("Lerro kantitatea?");
-        BufferedReader bf1 =new BufferedReader(new InputStreamReader(System.in));
-        int kant= Integer.parseInt(bf1.readLine());
+           File f1 = new File(".\\fitx1.txt");
 
-        System.out.println("Sartu karaktere-kateak:");
-        BufferedReader bf2 =new BufferedReader(new InputStreamReader(System.in));
-        Writer fw = new FileWriter(".\\fitx1.txt");
-        int lerroak=0;
-        while (kant>=lerroak){
-            lerroak++;
-            fw.write(String.valueOf(bf2));
-        }
+           System.out.println("Lerro kantitatea?");
+           BufferedReader bf1 = new BufferedReader(new InputStreamReader(System.in));
+           int kant = Integer.parseInt(bf1.readLine());
+
+
+           FileWriter fw = new FileWriter(".\\fitx1.txt");
+           BufferedWriter bw = new BufferedWriter(fw);
+
+
+           System.out.println("Sartu karaktere-kateak:");
+           for (int i = 0; i < kant; i++) {
+               String karak_katea = String.valueOf(bf1.readLine());
+               char[] bai = karak_katea.toCharArray();
+
+               for (int a = bai.length - 1; a >= 0; a--) {
+                   if (a <= 29) {
+                       bw.write(bai[a]);
+                   }
+               }
+               bw.newLine();
+           }
+
+           bf1.close();
+           bw.close();
+       }catch (IOException e){
+           System.err.println("Errorea");
+       }
+
     }
 }
