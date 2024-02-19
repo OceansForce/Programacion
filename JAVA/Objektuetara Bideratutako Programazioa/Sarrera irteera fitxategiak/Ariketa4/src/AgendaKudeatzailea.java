c7a038a363beb130b623fd2897c72ed7272c2154
OceansForce <julengarcia165@gmail.com>
2024-02-19 10:22:52 +0100

    4 ariketa try gehitzen

--- JAVA/Objektuetara Bideratutako Programazioa/Sarrera irteera fitxategiak/Ariketa4/src/AgendaKudeatzailea.java
+++ JAVA/Objektuetara Bideratutako Programazioa/Sarrera irteera fitxategiak/Ariketa4/src/AgendaKudeatzailea.java
@@ -7,39 +7,51 @@ public class AgendaKudeatzailea {
         Fitx_izena = fitx_izena;
     }
 
-    void erakutsi() throws IOException {
+    void erakutsi() {
+        try {
 
-        BufferedReader br= new BufferedReader(new FileReader(".\\"+Fitx_izena+".txt"));
-        String textua= br.readLine();
-        while (textua != null){
-            System.out.println(textua);
-            textua= br.readLine();
+            BufferedReader br = new BufferedReader(new FileReader(".\\" + Fitx_izena + ".txt"));
+            String textua = br.readLine();
+            while (textua != null) {
+                System.out.println(textua);
+                textua = br.readLine();
+            }
+            br.close();
+        }catch (IOException e){
+            System.err.println("Ezin da irakurri");
         }
-        br.close();
 
     }
     void gehitupertsona(Pertsona per) throws IOException {
-        BufferedWriter fw1= new BufferedWriter(new FileWriter(".\\"+Fitx_izena+".txt", true));
-        fw1.write(String.valueOf(per));
-        fw1.close();
+        try {
+            BufferedWriter fw1 = new BufferedWriter(new FileWriter(".\\" + Fitx_izena + ".txt", true));
+            fw1.write(String.valueOf(per));
+            fw1.close();
+        }catch (IOException e){
+            System.err.println("Ezin da gehitu pertsona");
+        }
     }
 
     String pertsonaBilatu(String pertsona) throws IOException {
+        try {
 
-        FileReader fr=new FileReader(".\\"+Fitx_izena+".txt");
-        BufferedReader bw= new BufferedReader(fr);
-        String textua= bw.readLine();
+            FileReader fr = new FileReader(".\\" + Fitx_izena + ".txt");
+            BufferedReader bw = new BufferedReader(fr);
+            String textua = bw.readLine();
 
-        while (textua != null) {
-            String[] arraya= textua.split("\t");
+            while (textua != null) {
+                String[] arraya = textua.split("\t");
 
-            if (arraya[0].equals("izena=" + pertsona)) {
-                return textua;
+                if (arraya[0].equals("izena=" + pertsona)) {
+                    return textua;
+                }
+                textua = bw.readLine();
             }
-            textua=bw.readLine();
+            bw.close();
+            fr.close();
+        }catch (IOException e){
+            System.err.println("Ezin da irakurri fitxategia");
         }
-        bw.close();
-        fr.close();
         return null;
     }
 }
