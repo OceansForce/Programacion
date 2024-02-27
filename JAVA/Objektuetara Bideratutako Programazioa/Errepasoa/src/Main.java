4b7f8688265b747cab070219dd86a7e232653f1b
OceansForce <julengarcia165@gmail.com>
2024-02-27 21:32:47 +0100

    Azterketa errepasoa

---
+++ JAVA/Objektuetara Bideratutako Programazioa/Errepasoa/src/Main.java
@@ -0,0 +1,25 @@
+import javax.imageio.IIOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> Al1= new ArrayList<>();
+        Al1.add("holla");
+        Al1.add(0,"holla");
+        Al1.remove(0);
+        Al1.remove("holla");
+        Al1.set(0, "holla2");
+        Al1.size();
+        Al1.isEmpty();
+        Al1.indexOf();
+        HashSet<String> As1= new HashSet<>();
+
+        HashMap<String,Integer> Hm1= new HashMap<String,Integer>();
+
+
+    }
+}
