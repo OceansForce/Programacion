0c407ece6433acca38a0e0257448037bf0e3b8ee
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-03-14 11:40:57 +0100

    Mugarria 1 egiten

--- JAVA/Interfaze Grafikoak/Mugarria1/src/Orria.java
+++ JAVA/Interfaze Grafikoak/Mugarria1/src/Orria.java
@@ -1,6 +1,4 @@
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.*;
 
 public class Orria extends JFrame  {
@@ -11,29 +9,26 @@ public class Orria extends JFrame  {
     public void erakutsi() {
         kaxak();
         orria();
+
     }
 
     private void orria() {
 
-        setLayout(null); //Layout absoluto
-        setTitle("Ejemplo 10"); //Título del JFrame
-        setSize(500, 250); //Dimensiones del JFrame
-        setResizable(false); //No redimensionable
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Cerrar proceso al cerrar ventana
-        setVisible(true); //Mostrar JFrame
+        setTitle("Ejemplo 10");
+        setSize(500, 250);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
     }
 
     private void kaxak(){
         kaxa1 = new JCheckBox("Katniss");
-        kaxa1.setBounds(230,0,150,30);
-        getContentPane().add(kaxa1, BorderLayout.CENTER);
-        add(kaxa1);
-
         kaxa2 = new JCheckBox("Peeta");
-        getContentPane().add(kaxa2, BorderLayout.CENTER);
-        kaxa2.setBounds(170,0,150,30);
-
+        getContentPane().add(kaxa1, BorderLayout.NORTH);
+        getContentPane().add(kaxa2, BorderLayout.NORTH);
+        add(kaxa1);
         add(kaxa2);
+
     }
 
     private void irudiak(){
