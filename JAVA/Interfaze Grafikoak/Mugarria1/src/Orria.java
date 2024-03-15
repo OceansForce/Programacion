e5defe864406a838796ad6af1b01cc9ec322cb88
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-03-15 12:49:25 +0100

    1 Mugarria egiten
    Irudia txertatzen

--- JAVA/Interfaze Grafikoak/Mugarria1/src/Orria.java
+++ JAVA/Interfaze Grafikoak/Mugarria1/src/Orria.java
@@ -4,10 +4,11 @@ import java.awt.*;
 public class Orria extends JFrame  {
 
     private JCheckBox kaxa1, kaxa2;
-    private ImageIcon img;
-
+    private JPanel p1;
+    private JPanel p2;
     public void erakutsi() {
         kaxak();
+        zentroa();
         orria();
 
     }
@@ -16,25 +17,37 @@ public class Orria extends JFrame  {
 
         setTitle("Ejemplo 10");
         setSize(500, 250);
-        setResizable(false);
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
     }
 
     private void kaxak(){
+
+        p1=new JPanel();
+        p1.setLayout(new FlowLayout(FlowLayout.CENTER));
         kaxa1 = new JCheckBox("Katniss");
         kaxa2 = new JCheckBox("Peeta");
-        getContentPane().add(kaxa1, BorderLayout.NORTH);
-        getContentPane().add(kaxa2, BorderLayout.NORTH);
-        add(kaxa1);
-        add(kaxa2);
+
+        p1.add(kaxa1);
+        p1.add(kaxa2);
+        p1.setBackground(Color.BLUE);
+
+        add(p1, BorderLayout.NORTH);
+    }
+
+    private void zentroa(){
+        p2= new JPanel();
+        p2.setLayout(new GridLayout(2,2));
+        ImageIcon icon = new ImageIcon(".\\distancia-pecoreo-abejas.png");
+
+        p2.add(new JLabel(icon));
+        p2.setBackground(Color.RED);
+        add(p2, BorderLayout.CENTER);
 
     }
 
-    private void irudiak(){
-        ImageIcon icon = new ImageIcon("./berserk.png");
-        Image irudia = icon.getImage();
-    }
 
 
 }
